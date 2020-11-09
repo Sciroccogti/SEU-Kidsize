45780e1b04c8ff428cb7f3634c1855d7e35aae47
Sciroccogti <scirocco_gti@yeah.net>
2020-11-09 14:11:22 +0800

    able to scan

--- src/control/src/main.cpp
+++ src/control/src/main.cpp
@@ -9,11 +9,14 @@
 #include <ros/package.h>
 #include <ros/ros.h>
 
+#include <common/datadef.hpp>
 #include <config/basic_parser.hpp>
 #include <fstream>
 #include <mutex>
 
 using namespace common;
+
+const char *GameState[]{"Initial", "Ready", "Set", "Playing", "Finish"};
 
 // searchBallTable = [(-90, 0), (-45, 0), (0, 0), (45, 0), (90, 0),
 //     (90, 30), (45, 30), (0, 30), (-45, 30), (-90, 30),
@@ -26,6 +29,8 @@ HeadAngles headAngles;
 ImuData imuData;
 GcInfo gcData;
 
+void imgResUpdate(const ImageResult::ConstPtr &p) { imgResult = *p; }
+
 void headUpdate(const HeadAngles::ConstPtr &p) { headAngles = *p; }
 
 void imuUpdate(const ImuData::ConstPtr &p) { imuData = *p; }
@@ -34,6 +39,7 @@ void gcUpdate(const GcInfo::ConstPtr &p) { gcData = *p; }
 
 int main(int argc, char **argv) {
     uint8_t id = 0;  // TODO: read from conf
+    int gcstattmp = -1;
     ros::init(argc, argv, "strategy");
     ros::NodeHandle node;
     ros::Publisher bodyTaskPublisher =
@@ -43,23 +49,52 @@ int main(int argc, char **argv) {
     ros::Publisher ledTaskPublisher = node.advertise<LedTask>("/task/led", 1);
     ros::Publisher playerInfoPublisher =
         node.advertise<PlayerInfo>("/sensor/1", 1);
+    ros::Subscriber imgResSubscriber =
+        node.subscribe("/result/vision/imgproc", 1, imgResUpdate);
     ros::Subscriber headSubcriber =
         node.subscribe("/sensor/head", 1, headUpdate);
     ros::Subscriber imuSubcriber = node.subscribe("/sensor/imu", 1, imuUpdate);
     ros::Subscriber gcSubscriber = node.subscribe("/sensor/gctrl", 1, gcUpdate);
 
-    std::string cfgpath = ros::package::getPath("config") + "/conf/";
-    std::string config_str = cfgpath + "conf.conf";
-    common::bpt::ptree pt;
-    common::get_tree_from_file(config_str, pt);
-    common::bpt::ptree tpt = pt.get_child("strategy");
+    // std::string cfgpath = ros::package::getPath("config") + "/conf/";
+    // std::string config_str = cfgpath + "config.conf";
+    // bpt::ptree pt;
+    // get_tree_from_file(config_str, pt);
+    // bpt::ptree ptstrategy = pt.get_child("strategy");
+    // bpt::ptree ptguard = ptstrategy.get_child("guard");
+    const float init_pos[2] = {-3.0, 0.0};
+    const float start_pos[2] = {-3.0, 3.0};
+    const float attack_range[2] = {-4.5, 0.0};
+
     ros::Rate rate(10);
 
     while (ros::ok()) {
         HeadTask htask;
         BodyTask btask;
         PlayerInfo pinfo;
+        btask.type = BodyTask::TASK_ACT;
+        // btask.count = 2;
+        // btask.step = 0.0;
+        btask.actname = "ready";
+        pinfo.id = id;
 
+        if (gcstattmp != gcData.state) {
+            gcstattmp = gcData.state;
+            ROS_INFO("Entering %s", GameState[gcstattmp]);
+        }
+
+        switch (gcstattmp) {
+            case GC_INITIAL:
+                btask.type = BodyTask::TASK_ACT;
+                btask.actname = "ready";
+                break;
+
+            default:
+                btask.type = BodyTask::TASK_ACT;
+                btask.actname = "reset";
+                htask.mode = htask.ModeScanBall;
+                break;
+        }
         headTaskPublisher.publish(htask);
         bodyTaskPublisher.publish(btask);
         playerInfoPublisher.publish(pinfo);
--- src/motion/src/motion.cpp
+++ src/motion/src/motion.cpp
@@ -1,22 +1,23 @@
-#include "scan/ScanEngine.hpp"
-#include "walk/WalkEngine.hpp"
+#include <bits/stdc++.h>
+#include <common/AddAngles.h>
 #include <common/BodyAngles.h>
-#include <common/HeadAngles.h>
 #include <common/BodyTask.h>
+#include <common/GetActions.h>
+#include <common/GetAngles.h>
+#include <common/HeadAngles.h>
 #include <common/HeadTask.h>
 #include <common/ImuData.h>
-#include <common/GetAngles.h>
-#include <common/AddAngles.h>
-#include <common/GetActions.h>
+#include <ros/ros.h>
 #include <std_srvs/Empty.h>
 #include <std_srvs/SetBool.h>
+
 #include <robot/action_engine.hpp>
 #include <robot/robot.hpp>
-#include <bits/stdc++.h>
-#include <ros/ros.h>
+
+#include "scan/ScanEngine.hpp"
+#include "walk/WalkEngine.hpp"
 
 using namespace Eigen;
-
 
 void UpdateBodyTask(const common::BodyTask::ConstPtr &p);
 common::BodyTask GetBodyTask();
@@ -24,9 +25,12 @@ void UpdateHeadTask(const common::HeadTask::ConstPtr &p);
 common::HeadTask GetHeadTask();
 void UpdateImu(const common::ImuData::ConstPtr &p);
 common::ImuData GetImuData();
-bool GetAnglesService(common::GetAngles::Request &req, common::GetAngles::Response &res);
-bool AddAnglesService(common::AddAngles::Request &req, common::AddAngles::Response &res);
-bool GetActionsService(common::GetActions::Request &req, common::GetActions::Response &res);
+bool GetAnglesService(common::GetAngles::Request &req,
+                      common::GetAngles::Response &res);
+bool AddAnglesService(common::AddAngles::Request &req,
+                      common::AddAngles::Response &res);
+bool GetActionsService(common::GetActions::Request &req,
+                       common::GetActions::Response &res);
 
 std::shared_ptr<robot::ActionEngine> actionEng;
 std::shared_ptr<WalkEngine> walkEng;
@@ -43,244 +47,219 @@ bool debug = false;
 std::deque<common::BodyAngles> bodyAngles;
 std::deque<common::HeadAngles> headAngles;
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "motion");
-  ros::NodeHandle node;
-  bool params_seted = false;
-  while (!params_seted && ros::ok())
-  {
-    try
-    {
-      ros::param::get("params", params_seted);
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "motion");
+    ros::NodeHandle node;
+    bool params_seted = false;
+    while (!params_seted && ros::ok()) {
+        try {
+            ros::param::get("params", params_seted);
+        } catch (const ros::InvalidNameException &e) {
+            ROS_WARN("%s", e.what());
+        }
+        usleep(100000);
     }
-    catch (const ros::InvalidNameException &e)
-    {
-      ROS_WARN("%s", e.what());
+    if (!params_seted) return 0;
+    std::string act_file, robot_file, offset_file, walk_file;
+    try {
+        ros::param::get("action_file", act_file);
+        ros::param::get("robot_file", robot_file);
+        ros::param::get("offset_file", offset_file);
+        ros::param::get("walk_file", walk_file);
+    } catch (ros::InvalidNameException &e) {
+        ROS_ERROR("%s", e.what());
+        return 0;
     }
-    usleep(100000);
-  }
-  if(!params_seted) return 0;
-  std::string act_file, robot_file, offset_file, walk_file;
-  try
-  {
-    ros::param::get("action_file", act_file);
-    ros::param::get("robot_file", robot_file);
-    ros::param::get("offset_file", offset_file);
-    ros::param::get("walk_file", walk_file);
-  }
-  catch (ros::InvalidNameException &e)
-  {
-    ROS_ERROR("%s", e.what());
+    maxwell = std::make_shared<robot::Robot>(robot_file, offset_file);
+    actionEng = std::make_shared<robot::ActionEngine>(act_file, maxwell);
+    walkEng = std::make_shared<WalkEngine>(walk_file, maxwell);
+    scanEng = std::make_shared<ScanEngine>();
+
+    ros::Subscriber bodySub = node.subscribe("/task/body", 1, UpdateBodyTask);
+    ros::Subscriber headSub = node.subscribe("/task/head", 1, UpdateHeadTask);
+    ros::Subscriber imuSub = node.subscribe("/sensor/imu", 1, UpdateImu);
+    ros::ServiceServer getAglSrv =
+        node.advertiseService("/get_angles", GetAnglesService);
+    ros::ServiceServer addAglSrv =
+        node.advertiseService("/add_angles", AddAnglesService);
+    ros::ServiceServer getActsSrv =
+        node.advertiseService("/get_actions", GetActionsService);
+
+    double phase = 0.0;
+    bool isWalking = false;
+    ros::Rate loop_rate(100);
+    std::vector<common::BodyAngles> angles = actionEng->runAction("ready");
+    bodyAngles.insert(bodyAngles.end(), angles.begin(), angles.end());
+    while (ros::ok()) {
+        if (debug) {
+            ros::spinOnce();
+            continue;
+        }
+        if (headAngles.size() < 5) {
+            auto task = GetHeadTask();
+            auto imu = GetImuData();
+            common::HeadAngles ha;
+            if (imu.fall != imu.FALL_NONE) {
+                ha.yaw = 90;
+                headAngles.push_back(ha);
+            } else if (task.mode == task.ModeLookAt) {
+                ha.pitch = task.pitch;
+                ha.yaw = task.yaw;
+                headAngles.push_back(ha);
+            } else {
+                std::vector<common::HeadAngles> vheadAngles =
+                    scanEng->runScan(task);
+                headAngles.insert(headAngles.end(), vheadAngles.begin(),
+                                  vheadAngles.end());
+            }
+        }
+        if (bodyAngles.size() < 5) {
+            auto task = GetBodyTask();
+            auto imu = GetImuData();
+            angles.clear();
+            if (imu.fall == imu.FALL_FORWARD) {
+                angles = actionEng->runAction("front_getup");
+            } else if (imu.fall == imu.FALL_BACKWARD) {
+                angles = actionEng->runAction("back_getup");
+            } else if (imu.fall == imu.FALL_LEFT) {
+                angles = actionEng->runAction("front_getup");
+            } else if (imu.fall == imu.FALL_RIGHT) {
+                angles = actionEng->runAction("front_getup");
+            } else {
+                if (task.type == common::BodyTask::TASK_WALK) {
+                    if (task.count > 0) {
+                        if (!isWalking) {
+                            angles = walkEng->runWalk(Vector3d(0.0, 0.0, 0.0),
+                                                      2, phase);
+                            isWalking = true;
+                        } else {
+                            angles = walkEng->runWalk(
+                                Vector3d(task.step, task.lateral, task.turn),
+                                task.count, phase);
+                        }
+                    } else {
+                        if (isWalking) {
+                            angles = walkEng->runWalk(Vector3d(0.0, 0.0, 0.0),
+                                                      0, phase);
+                            isWalking = false;
+                        }
+                    }
+                } else if (task.type == common::BodyTask::TASK_ACT) {
+                    if (task.count >= 0) {
+                        if (isWalking) {
+                            angles = walkEng->runWalk(Vector3d(0.0, 0.0, 0.0),
+                                                      0, phase);
+                            isWalking = false;
+                        }
+                        angles = actionEng->runAction(task.actname);
+                    }
+                } else {
+                    if (isWalking) {
+                        angles =
+                            walkEng->runWalk(Vector3d(0.0, 0.0, 0.0), 0, phase);
+                        isWalking = false;
+                    }
+                }
+            }
+            bodyAngles.insert(bodyAngles.end(), angles.begin(), angles.end());
+        }
+        ros::spinOnce();
+    }
     return 0;
-  }
-  maxwell = std::make_shared<robot::Robot>(robot_file, offset_file);
-  actionEng = std::make_shared<robot::ActionEngine>(act_file, maxwell);
-  walkEng = std::make_shared<WalkEngine>(walk_file, maxwell);
-  scanEng = std::make_shared<ScanEngine>();
+}
 
-  ros::Subscriber bodySub = node.subscribe("/task/body", 1, UpdateBodyTask);
-  ros::Subscriber headSub = node.subscribe("/task/head", 1, UpdateHeadTask);
-  ros::Subscriber imuSub = node.subscribe("/sensor/imu", 1, UpdateImu);
-  ros::ServiceServer getAglSrv = node.advertiseService("/get_angles", GetAnglesService);
-  ros::ServiceServer addAglSrv = node.advertiseService("/add_angles", AddAnglesService);
-  ros::ServiceServer getActsSrv = node.advertiseService("/get_actions", GetActionsService);
+void UpdateBodyTask(const common::BodyTask::ConstPtr &p) {
+    std::lock_guard<std::mutex> lk(bodyMutex);
+    bodyTask = *p;
+}
 
-  double phase = 0.0;
-  bool isWalking = false;
-  ros::Rate loop_rate(100);
-  std::vector<common::BodyAngles> angles = actionEng->runAction("ready");
-  bodyAngles.insert(bodyAngles.end(), angles.begin(), angles.end());
-  while (ros::ok())
-  {
-    if(debug) {
-      ros::spinOnce();
-      continue;
+common::BodyTask GetBodyTask() {
+    std::lock_guard<std::mutex> lk(bodyMutex);
+    auto ret = bodyTask;
+    bodyTask.count = -1;
+    return ret;
+}
+
+void UpdateHeadTask(const common::HeadTask::ConstPtr &p) {
+    std::lock_guard<std::mutex> lk(headMutex);
+    headTask = *p;
+}
+
+common::HeadTask GetHeadTask() {
+    std::lock_guard<std::mutex> lk(headMutex);
+    return headTask;
+}
+
+void UpdateImu(const common::ImuData::ConstPtr &p) {
+    std::lock_guard<std::mutex> lk(imuMutex);
+    imuData = *p;
+}
+
+common::ImuData GetImuData() {
+    std::lock_guard<std::mutex> lk(imuMutex);
+    return imuData;
+}
+
+bool GetAnglesService(common::GetAngles::Request &req,
+                      common::GetAngles::Response &res) {
+    // ROS_INFO("getangles");
+    bodyMutex.lock();
+    if (!bodyAngles.empty()) {
+        maxwell->set_body(bodyAngles.front());
+        bodyAngles.pop_front();
     }
-    if (headAngles.size() < 5)
-    {
-      auto task = GetHeadTask();
-      common::HeadAngles ha;
-      ha.pitch = task.pitch;
-      ha.yaw = task.yaw;
-      headAngles.push_back(ha);
+    bodyMutex.unlock();
+    headMutex.lock();
+    if (!headAngles.empty()) {
+        maxwell->set_head(headAngles.front());
+        headAngles.pop_front();
     }
-    if (bodyAngles.size() < 5)
-    {
-      auto task = GetBodyTask();
-      auto imu = GetImuData();
-      angles.clear();
-      if (imu.fall == imu.FALL_FORWARD)
-      {
-        angles = actionEng->runAction("front_getup");
-      }
-      else if (imu.fall == imu.FALL_BACKWARD)
-      {
-        angles = actionEng->runAction("back_getup");
-      }
-      else if (imu.fall == imu.FALL_LEFT)
-      {
-        angles = actionEng->runAction("left_getup");
-      }
-      else if (imu.fall == imu.FALL_RIGHT)
-      {
-        angles = actionEng->runAction("right_getup");
-      }
-      else
-      {
-        if (task.type == common::BodyTask::TASK_WALK)
-        {
-          if (task.count > 0)
-          {
-            if (!isWalking)
-            {
-              angles = walkEng->runWalk(Vector3d(0.0, 0.0, 0.0), 2, phase);
-              isWalking = true;
-            }
-            else
-            {
-              angles = walkEng->runWalk(Vector3d(task.step, task.lateral, task.turn), task.count, phase);
-            }
-          }
-          else
-          {
-            if (isWalking)
-            {
-              angles = walkEng->runWalk(Vector3d(0.0, 0.0, 0.0), 0, phase);
-              isWalking = false;
-            }
-          }
+    headMutex.unlock();
+    if (req.player == "real") {
+        int sid = maxwell->joint_start_id();
+        res.start_id = sid;
+        for (int i = 0; i < maxwell->joint_count(); i++) {
+            auto joint = maxwell->get_joint(sid + i);
+            float deg = (joint->current_deg + joint->offset) * joint->inverse;
+            res.degs.push_back(deg);
         }
-        else if (task.type == common::BodyTask::TASK_ACT)
-        {
-          if (task.count >= 0)
-          {
-            if (isWalking)
-            {
-              angles = walkEng->runWalk(Vector3d(0.0, 0.0, 0.0), 0, phase);
-              isWalking = false;
-            }
-            angles = actionEng->runAction(task.actname);
-          }
-        }
-        else
-        {
-          if (isWalking)
-          {
-            angles = walkEng->runWalk(Vector3d(0.0, 0.0, 0.0), 0, phase);
-            isWalking = false;
-          }
-        }
-      }
-      bodyAngles.insert(bodyAngles.end(), angles.begin(), angles.end());
+    } else {
+        res.head.yaw = maxwell->get_joint("jhead1")->current_deg;
+        res.head.pitch = maxwell->get_joint("jhead2")->current_deg;
+        res.body.left_shoulder = maxwell->get_joint("jlshoulder1")->current_deg;
+        res.body.left_elbow = maxwell->get_joint("jlelbow")->current_deg;
+        res.body.right_shoulder =
+            maxwell->get_joint("jrshoulder1")->current_deg;
+        res.body.right_elbow = maxwell->get_joint("jrelbow")->current_deg;
+
+        res.body.left_hip_yaw = maxwell->get_joint("jlhip3")->current_deg;
+        res.body.left_hip_roll = maxwell->get_joint("jlhip2")->current_deg;
+        res.body.left_hip_pitch = maxwell->get_joint("jlhip1")->current_deg;
+        res.body.left_knee = maxwell->get_joint("jlknee")->current_deg;
+        res.body.left_ankle_pitch = maxwell->get_joint("jlankle2")->current_deg;
+        res.body.left_ankle_roll = maxwell->get_joint("jlankle1")->current_deg;
+
+        res.body.right_hip_yaw = maxwell->get_joint("jrhip3")->current_deg;
+        res.body.right_hip_roll = maxwell->get_joint("jrhip2")->current_deg;
+        res.body.right_hip_pitch = maxwell->get_joint("jrhip1")->current_deg;
+        res.body.right_knee = maxwell->get_joint("jrknee")->current_deg;
+        res.body.right_ankle_pitch =
+            maxwell->get_joint("jrankle2")->current_deg;
+        res.body.right_ankle_roll = maxwell->get_joint("jrankle1")->current_deg;
     }
-    ros::spinOnce();
-  }
-  return 0;
+    return true;
 }
 
-void UpdateBodyTask(const common::BodyTask::ConstPtr &p)
-{
-  std::lock_guard<std::mutex> lk(bodyMutex);
-  bodyTask = *p;
-}
-
-common::BodyTask GetBodyTask()
-{
-  std::lock_guard<std::mutex> lk(bodyMutex);
-  auto ret = bodyTask;
-  bodyTask.count = -1;
-  return ret;
-}
-
-void UpdateHeadTask(const common::HeadTask::ConstPtr &p)
-{
-  std::lock_guard<std::mutex> lk(headMutex);
-  headTask = *p;
-}
-
-common::HeadTask GetHeadTask()
-{
-  std::lock_guard<std::mutex> lk(headMutex);
-  return headTask;
-}
-
-void UpdateImu(const common::ImuData::ConstPtr &p)
-{
-  std::lock_guard<std::mutex> lk(imuMutex);
-  imuData = *p;
-}
-
-common::ImuData GetImuData()
-{
-  std::lock_guard<std::mutex> lk(imuMutex);
-  return imuData;
-}
-
-bool GetAnglesService(common::GetAngles::Request &req, common::GetAngles::Response &res)
-{
-  //ROS_INFO("getangles");
-  bodyMutex.lock();
-  if (!bodyAngles.empty())
-  {
-    maxwell->set_body(bodyAngles.front());
-    bodyAngles.pop_front();
-  }
-  bodyMutex.unlock();
-  headMutex.lock();
-  if (!headAngles.empty())
-  {
-    maxwell->set_head(headAngles.front());
-    headAngles.pop_front();
-  }
-  headMutex.unlock();
-  if(req.player == "real")
-  {
-    int sid = maxwell->joint_start_id();
-    res.start_id = sid;
-    for(int i=0; i<maxwell->joint_count(); i++)
-    {
-      auto joint = maxwell->get_joint(sid+i);
-      float deg = (joint->current_deg + joint->offset) * joint->inverse;
-      res.degs.push_back(deg);
-    }
-  }
-  else
-  {
-    res.head.yaw = maxwell->get_joint("jhead1")->current_deg;
-    res.head.pitch = maxwell->get_joint("jhead2")->current_deg;
-    res.body.left_shoulder = maxwell->get_joint("jlshoulder1")->current_deg;
-    res.body.left_elbow = maxwell->get_joint("jlelbow")->current_deg;
-    res.body.right_shoulder = maxwell->get_joint("jrshoulder1")->current_deg;
-    res.body.right_elbow = maxwell->get_joint("jrelbow")->current_deg;
-
-    res.body.left_hip_yaw = maxwell->get_joint("jlhip3")->current_deg;
-    res.body.left_hip_roll = maxwell->get_joint("jlhip2")->current_deg;
-    res.body.left_hip_pitch = maxwell->get_joint("jlhip1")->current_deg;
-    res.body.left_knee = maxwell->get_joint("jlknee")->current_deg;
-    res.body.left_ankle_pitch = maxwell->get_joint("jlankle2")->current_deg;
-    res.body.left_ankle_roll = maxwell->get_joint("jlankle1")->current_deg;
-
-    res.body.right_hip_yaw = maxwell->get_joint("jrhip3")->current_deg;
-    res.body.right_hip_roll = maxwell->get_joint("jrhip2")->current_deg;
-    res.body.right_hip_pitch = maxwell->get_joint("jrhip1")->current_deg;
-    res.body.right_knee = maxwell->get_joint("jrknee")->current_deg;
-    res.body.right_ankle_pitch = maxwell->get_joint("jrankle2")->current_deg;
-    res.body.right_ankle_roll = maxwell->get_joint("jrankle1")->current_deg;
-  }
-  return true;
-}
-
-bool AddAnglesService(common::AddAngles::Request &req, common::AddAngles::Response &res)
-{
+bool AddAnglesService(common::AddAngles::Request &req,
+                      common::AddAngles::Response &res) {
     ROS_INFO("AddAnglesService");
     bodyAngles.insert(bodyAngles.end(), req.angles.begin(), req.angles.end());
     debug = true;
     return true;
 }
 
-bool GetActionsService(common::GetActions::Request &req, common::GetActions::Response &res)
-{
-  res.actions = actionEng->getActions();
-  return true;
+bool GetActionsService(common::GetActions::Request &req,
+                       common::GetActions::Response &res) {
+    res.actions = actionEng->getActions();
+    return true;
 }
